85b848813f97e5a2874426bce496ea58de40c7d3
rohan-gofro <rohan.gupta@gofro.com>
2018-01-21 16:12:52 +0530

    FindDigits

---
+++ src/ExtraLongFactorials.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ExtraLongFactorials {
+
+    static void extraLongFactorials(int n) {
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        extraLongFactorials(n);
+        in.close();
+    }
+}
--- src/FindDigits.java
+++ src/FindDigits.java
@@ -3,13 +3,22 @@ import java.util.Scanner;
 public class FindDigits {
 
     static int findDigits(int n) {
-        // Complete this function
+        String num = "" + n;
+        int count = 0;
+        for (int i = 0; i < num.length(); i++) {
+            if (num.charAt(i) != '0') {
+                if (n % Integer.parseInt("0" + num.charAt(i)) == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
+        for (int a0 = 0; a0 < t; a0++) {
             int n = in.nextInt();
             int result = findDigits(n);
             System.out.println(result);
