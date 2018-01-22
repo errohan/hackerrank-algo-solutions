8b2bbf365a3dd25d62f3af2ab689077b0d3a5152
Rohan Gupta <rohangupta.cs@gmail.com>
2018-01-22 15:40:01 +0530

    climbing leaderboard problem

--- src/ClimbingLeaderBoard.java
+++ src/ClimbingLeaderBoard.java
@@ -4,16 +4,54 @@ public class ClimbingLeaderBoard {
 
     static int[] climbingLeaderboard(int scores[], int[] alice) {
         int aliceRank[] = new int[alice.length];
-        //binary search
-        return null;
+        int rank;
+        for (int i = 0;i<alice.length;i++) {
+            rank = bsearch(scores,alice[i]);
+            if(scores[rank] == alice[i]){
+                aliceRank[i] = rank+1;
+            }
+            else if(scores[rank] > alice[i]){
+                aliceRank[i] = rank+2;
+            }
+            else {
+                aliceRank[i] = rank+1;
+            }
+        }
+        return aliceRank;
     }
+
+    public static int bsearch(int a[] , int n){
+        int low = 0 , high = a.length-1;
+        int mid = -1;
+        while(low < high){
+            mid = (low + high)/2;
+            if(a[mid] > n){
+                low = mid + 1;
+            }
+            else if(a[mid] < n){
+                high = mid -1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return low;
+    }
+
+
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[] scores = new int[n];
+        Set<Integer> scoreSet = new LinkedHashSet<>();
         for(int scores_i = 0; scores_i < n; scores_i++){
-            scores[scores_i] = in.nextInt();
+            scoreSet.add(in.nextInt());
+        }
+        int []scores = new int[scoreSet.size()];
+        int c = 0;
+        for(int s : scoreSet){
+            scores[c] = s;
+            c++;
         }
         int m = in.nextInt();
         int[] alice = new int[m];
