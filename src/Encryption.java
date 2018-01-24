6f2d662d49a70877e1468daaa12b8bdb0d39e347
Rohan Gupta <rohangupta.cs@gmail.com>
2018-01-24 12:14:25 +0530

    Encryption

---
+++ src/Encryption.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/*
+  author  : rohan.gupta
+  created : 24/1/18
+*/
+public class Encryption {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.next();
+        int row = (int) Math.floor(Math.sqrt(inputString.length()));
+        int col = (int) Math.ceil(Math.sqrt(inputString.length()));
+        if(row*col < inputString.length()){
+            row = (int) Math.ceil(Math.sqrt(inputString.length()));
+        }
+        String textBlock[] = new String[row];
+        for(int i=0;i<row;i++){
+            if(i==row-1){
+                textBlock[i]=inputString.substring(i*col,inputString.length());
+            }
+            else {
+            textBlock[i]=inputString.substring(i*col,(i+1)*col);
+            }
+        }
+        for(int i = 0;i<col;i++){
+            for(int j = 0;j<row;j++){
+                if(i<=textBlock[j].length()-1 )
+                System.out.print(textBlock[j].charAt(i));
+            }
+            System.out.print(" ");
+        }
+    }
+}
