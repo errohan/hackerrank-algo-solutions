ddfcf6e69bbb88b1d26182154a761ab106d56ea9
Rohan Gupta <rohangupta.cs@gmail.com>
2018-02-01 22:19:14 +0530

    Lisa Work book solved

---
+++ src/LisaWorkBook.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/*
+  author  : rohan.gupta
+  created : 1/2/18
+*/
+public class LisaWorkBook {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfChapters = sc.nextInt();
+        int capacityOfAPage = sc.nextInt();
+        int []questionsCountOfEachChapter = new int[numberOfChapters];
+        for(int i = 0;i<numberOfChapters;i++){
+            questionsCountOfEachChapter[i] = sc.nextInt();
+        }
+        System.out.println(getSpecialProblemsCount(numberOfChapters,capacityOfAPage,questionsCountOfEachChapter));
+    }
+
+    private static int getSpecialProblemsCount(int numberOfChapters, int capacityOfAPage, int[] questionsCountOfEachChapter) {
+        int specialProblemCount = 0;
+        int currentPageNumber = 1;
+        for(int i = 0;i<numberOfChapters;i++){
+            int startQuestion = 1;
+            while(startQuestion<=questionsCountOfEachChapter[i]){
+                int endQuestion = (startQuestion + capacityOfAPage -1)<=questionsCountOfEachChapter[i]?(startQuestion + capacityOfAPage -1):questionsCountOfEachChapter[i];
+                if(startQuestion <= currentPageNumber && currentPageNumber <= endQuestion){
+                    specialProblemCount++;
+                }
+                currentPageNumber++;
+                startQuestion = endQuestion + 1;
+            }
+        }
+        return specialProblemCount;
+    }
+}
---
+++ src/ServiceLane.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/*
+  author  : rohan.gupta
+  created : 1/2/18
+*/
+public class ServiceLane {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int serviceLaneLength = sc.nextInt();
+        int testCases = sc.nextInt();
+        int []serviceLane = new int[serviceLaneLength];
+        for(int i = 0;i<serviceLaneLength;i++){
+            serviceLane[i]=sc.nextInt();
+        }
+        for(int i = 0; i<testCases;i++){
+            int entryPoint = sc.nextInt();
+            int exitPoint = sc.nextInt();
+            int minWidth = 3;
+            for(int x = entryPoint;x<=exitPoint;x++){
+                if(minWidth > serviceLane[x]){
+                    minWidth=serviceLane[x];
+                }
+            }
+            System.out.println(minWidth);
+        }
+    }
+}
