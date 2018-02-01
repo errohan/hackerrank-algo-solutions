63f22886e8305dd55a5bc63325e5a4f2ae0dbd59
Rohan Gupta <rohangupta.cs@gmail.com>
2018-02-01 13:10:52 +0530

    append and delete fixed

--- src/AppendAndDelete.java
+++ src/AppendAndDelete.java
@@ -2,40 +2,47 @@
 import java.util.*;
 public class AppendAndDelete {
 
-    static String appendAndDelete(String s, String t, int k) {
-        if(s.equals(t)){
-            if(k>=2*s.length() || k%2==0){
-                return "Yes";
-            }
-        }
-        int firstMismatchIndex = firstMismatch(s,t);
-        int nmcInS = s.length() - firstMismatchIndex;
-        int nmcInT = t.length() - firstMismatchIndex;
-
-        boolean cond1 = nmcInS + nmcInT == k;
-        boolean cond2 = (nmcInS + nmcInT < k && (nmcInS + nmcInT -k)%2==0);
-        boolean cond3 = s.length() + t.length() <= k;
-
-        return cond1||cond2||cond3?"Yes":"No";
-    }
-
-    static int firstMismatch(String s1,String s2){
-        int mismatch = -1;
-        for(int i = 0;i<Math.min(s1.length(),s2.length());i++){
-            if(s1.charAt(i)!=s2.charAt(i)){
-                return i;
-            }
-        }
-        return mismatch;
-    }
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s = in.next();
-        String t = in.next();
-        int k = in.nextInt();
-        String result = appendAndDelete(s, t, k);
-        System.out.println(result);
-        in.close();
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String t = sc.next();
+        int k = sc.nextInt();
+
+        if(isAppendAndDeletePossible(s,t,k)){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+
+    }
+
+    private static boolean isAppendAndDeletePossible(String s, String t, int k) {
+        if(s.equals(t)){
+            if(k >= s.length()*2 || k%2==0)
+                return true;
+        }
+        else {
+            if(k>= s.length() + t.length())
+                return true;
+            int matchingCharacters = getMatchingCharacters(s,t);
+            int effort = s.length()+t.length() - (2*matchingCharacters);
+            if(effort <=k && (k-effort)%2==0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int getMatchingCharacters(String s, String t) {
+        int matchingChars = 0;
+        for(int i = 0;i<Math.min(s.length(),t.length());i++){
+            if(s.charAt(i) == t.charAt(i)){
+                matchingChars++;
+            }
+            else break;
+        }
+        return matchingChars;
     }
 }
