612ec93a269eb2a6875e2bff13ea2aae8b2ac94d
Rohan Gupta <rohangupta.cs@gmail.com>
2019-04-20 15:36:39 +0530

    Happy lady bugs and 3d surface area all test cases passed

--- src/implementations/HappyLadybugsII.java
+++ src/implementations/HappyLadybugsII.java
@@ -11,22 +11,35 @@ public class HappyLadybugsII {
 
     public static boolean happyLadyBugs(String s){
         Map<Character,Integer> map = new HashMap<>();
-        for(int i = 0;i<s.length();i++){
+        boolean happy = true;
+        char lastChar = s.charAt(0);
+        int count = 1;
+        map.put(s.charAt(0),1);
+        for(int i = 1;i<s.length();i++){
+            char cuurentChar = s.charAt(i);
+            if(happy && lastChar!=cuurentChar){
+                if(count==1) happy=false;
+                else if(map.containsKey(cuurentChar)) happy=false;
+                else{
+                    lastChar=cuurentChar;
+                    count=1;
+                }
+            }else count++;
+
             if(map.get(s.charAt(i))==null){
                 map.put(s.charAt(i),1);
             }else {
                 map.put(s.charAt(i),map.get(s.charAt(i))+1);
             }
         }
-        if(map.containsKey('_')){
-            map.remove("_");
-            return map.values().contains(1);
-        }else {
-            return false;
+        if((count!=1 && happy && lastChar!='_') || map.containsKey('_')){
+            map.remove('_');
+            return happy || !map.values().contains(1);
         }
+        return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(happyLadyBugs("RBY_YBR"));
+        System.out.println(happyLadyBugs("AABBC"));
     }
 }
